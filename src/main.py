5ddb817c0d8edb064f2f27074692a23f6c0d3590
phelph <philipsteffenprivat@gmail.com>
2023-10-11 16:43:39 +0200

    small main changes

--- src/main.py
+++ src/main.py
@@ -5,26 +5,25 @@ if __name__ == "__main__":
     print("Welcome to my little text based game!\n")
     print("At first you have to create a new character!")
 
-    playwe_1 = PlayerCreator().create_player()
+    player_1 = PlayerCreator().create_player()
 
-    # player_1 = Player(input("Name your Character!: "))
-    # # player_1.gain_exp(90)p
+    player_1.show_exp()
+    enemy_one = Enemy("Demon", player_1.level)
 
-    # player_1.show_exp()
-    # enemy_one = Enemy("Diablo", 1)
+    print(
+        f"A {enemy_one.type} (Level {enemy_one.lvl}, HP: {enemy_one.hp}) has appeared out of a dark corner!\n"
+    )
 
-    # print(f"An Enemy named {enemy_one.name} (Level {enemy_one.lvl}) has appeared!\n")
+    while True:
+        choice = input("Would you like to attack? (y/n): ")
 
-    # while True:
-    #     choice = input("Would you like to attack? (y/n): ")
+        if choice not in ["y", "n"]:
+            print("Please enter either 'y' or 'n'")
+        else:
+            break
 
-    #     if choice not in ["y", "n"]:
-    #         print("Please enter either 'y' or 'n'")
-    #     else:
-    #         break
+    if choice == "y":
+        player_1.attack(enemy_one)
 
-    # if choice == "y":
-    #     player_1.attack(enemy_one)
-
-    # elif choice == "n":
-    #     print(f"{player_1.name} has cowardly run away!\n")
+    elif choice == "n":
+        print(f"{player_1.name} has cowardly run away!\n")
