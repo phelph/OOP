6ef0041580382c399d6be235c3ad1564bd28f322
phelph <philipsteffenprivat@gmail.com>
2023-11-15 13:27:30 +0100

    added stats class with 2 methods

---
+++ src/classes/stats.py
@@ -0,0 +1,52 @@
+from dataclasses import dataclass, field
+
+
+@dataclass
+class Stats:
+    stats: dict = field(
+        default_factory=lambda: {
+            "strength": 5,
+            "dexterity": 5,
+            "arcane": 5,
+            "intelligence": 5,
+        }
+    )
+    spendable_points: int = 5
+
+    def print_stats(self) -> None:
+        print(f"Your current stats:\n")
+        for index, (stat, value) in enumerate(self.stats.items(), start=1):
+            print(f"{index}. {stat.capitalize()}: {value}")
+
+    def upgrade_stats(self) -> None:
+        while self.spendable_points > 0:
+            print(f"You have {self.spendable_points} spendable stat point left.\n")
+            self.print_stats()
+            try:
+                choice = int(
+                    input(
+                        "\nEnter the corresponding number of the stat you want to upgrade: "
+                    )
+                )
+                chosen_stat = list(self.stats.keys())[choice - 1]
+
+                points_to_add = int(
+                    input(
+                        f"\nHow many points ({self.spendable_points} left) do you want to add to {chosen_stat}? "
+                    )
+                )
+                if 0 < points_to_add <= self.spendable_points:
+                    self.stats[chosen_stat] += points_to_add
+                    self.spendable_points -= points_to_add
+                else:
+                    print("Invalid number of points. Please try again.\n")
+            except (IndexError, ValueError):
+                print("Invalid selection. Please try again.\n")
+
+        print("You have used all your spendable points.\n")
+        self.print_stats()
+
+
+# Example of using the class
+player_stats = Stats()
+player_stats.upgrade_stats()
