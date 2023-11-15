7f749b070da74fddaa821f6984f4304ae19e311c
phelph <philipsteffenprivat@gmail.com>
2023-11-15 17:05:48 +0100

    testing

--- src/main.py
+++ src/main.py
@@ -3,10 +3,11 @@ import classes
 if __name__ == "__main__":
     print("Welcome to my little text based game!\n")
     print("At first you have to create a new character!")
+    creator = classes.PlayerCreator()
+    player_1 = creator.create_player()
 
-    player_1 = classes.PlayerCreator().create_player()
-
-    player_1.show_exp()
+    player_1.stats.print_stats()
+    player_1.equipped_weapon.print_name_power()
 
     # player_1.take_dmg(100)
     # player_1.take_dmg(30)
